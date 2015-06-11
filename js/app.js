0b05f2a592fd050d145bab78d2fb8c7a1746bb51
dariush abbasi <dariushem@yahoo.com>
2015-06-11 12:04:35 +0430

    init simple parser

---
+++ js/app.js
@@ -0,0 +1,14 @@
+function showr(){
+	var blank_rubai = document.getElementById("rubai").value;
+	
+	var rubai = blank_rubai.split(/\r?\n/);
+
+	var formatted_rubai = {
+		one		: rubai[0],
+		two		: rubai[1],
+		three	: rubai[2],
+		four 	: rubai[3]
+	}
+
+	document.getElementById('output').innerHTML = JSON.stringify(formatted_rubai);
+}
